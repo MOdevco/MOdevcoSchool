86ea0d8fdb885a88b4b7f7622196e28757c21a83
MOdevco <modevco05@gmail.com>
2023-07-29 23:22:07 +0500

    delete hii file

--- src/pages/hii.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Hii = () => {
-  return (
-    <div>Hii</div>
-  )
-}
-
-export default Hii
---
+++ src/pages/register.jsx
@@ -0,0 +1,8 @@
+import { Box } from '@chakra-ui/react'
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
