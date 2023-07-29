c32e8f5ffd7124ed5e4aefec7ca7e15431eb4cff
modevco <modevco05@gmail.com>
2023-07-29 14:32:51 +0500

    fdsfs

--- src/pages/html18dars.jsx
+++ src/pages/html18dars.jsx
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Html18dars = () => {
   return (
     <div>
-      Odina qalesan man zor ozizchi
+      Odina qalesan man zor ozizchi ha yaxshida
     </div>
   )
 }
